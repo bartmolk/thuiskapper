d4ff4481a1b6d4256c3f00180d6dd6ff8081608a
Bernardus Molkenboer <bernardusmolkenboer@MacBook-Pro-van-Bernardus.local>
2021-08-07 13:09:27 +0200

    Updates

---
+++ precache-manifest.0f92b0f86a173b66bb9e8c1301eb563f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff2af4d39f3183ac524a",
+    "url": "/thuiskapper/static/css/main.5c994be6.chunk.css"
+  },
+  {
+    "revision": "ff2af4d39f3183ac524a",
+    "url": "/thuiskapper/static/js/main.ff2af4d3.chunk.js"
+  },
+  {
+    "revision": "d3635b4b1fd768e41bb5",
+    "url": "/thuiskapper/static/css/1.1f9e374e.chunk.css"
+  },
+  {
+    "revision": "d3635b4b1fd768e41bb5",
+    "url": "/thuiskapper/static/js/1.d3635b4b.chunk.js"
+  },
+  {
+    "revision": "9a9d8bf6bdbfe78aec87",
+    "url": "/thuiskapper/static/js/runtime~main.9a9d8bf6.js"
+  },
+  {
+    "revision": "a05653efb8da97c479966ef1c8a23e2a",
+    "url": "/thuiskapper/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/social-network/precache-manifest.8997140856635e11c813839b219b82dc.js"
+  "/thuiskapper/precache-manifest.0f92b0f86a173b66bb9e8c1301eb563f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/social-network/index.html", {
+workbox.routing.registerNavigationRoute("/thuiskapper/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
